b3ad8645fe15c26d15ca0ca44707dd3782de3706
D4C-lolu <dareolumideisfly@gmail.com>
2020-07-22 05:06:38 +0100

    Updates

---
+++ precache-manifest.142a4fa0bd831d364d9c006c7ec01511.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b50f015fdbc8106d4a28fb579840c9a1",
+    "url": "/Book-Store-with-Reducers/index.html"
+  },
+  {
+    "revision": "43ede39b724334258615",
+    "url": "/Book-Store-with-Reducers/static/css/main.6a062c54.chunk.css"
+  },
+  {
+    "revision": "fa37f865dbfa8a53eb95",
+    "url": "/Book-Store-with-Reducers/static/js/2.f1b28e9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Book-Store-with-Reducers/static/js/2.f1b28e9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43ede39b724334258615",
+    "url": "/Book-Store-with-Reducers/static/js/main.b9bff9d4.chunk.js"
+  },
+  {
+    "revision": "12b7c22e7d721c7afcf3",
+    "url": "/Book-Store-with-Reducers/static/js/runtime-main.921e1106.js"
+  }
+]);
